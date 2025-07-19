53c5a27d8e675503c483382c84afae956a87e0d5
realizer <ipreet35232@gmail.com>
2025-07-19 08:04:47 +0530

    completed purge command

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder, MessageFlags, SlashCommandBuilder } from "discord.js";
+import { Collection, EmbedBuilder, MessageFlags, SlashCommandBuilder } from "discord.js";
 
 const create = () => {
     const command = new SlashCommandBuilder().setName("purge").setDescription("deletes messages from channel")
@@ -12,10 +12,22 @@ const invoke = async (interaction) => {
         if (count < 1 || count > 100) {
             return interaction.reply("Please provide a valid count between 1 and 100");
         }
-        await interaction.reply({ content: `Deleting ${count} message(s)`, flags: MessageFlags.Ephemeral });
+        await interaction.reply({ content: `Deleting ${count} messages...`, flags: MessageFlags.Ephemeral });
         try {
             const deleted = await interaction.channel.bulkDelete(count, true);
-            await interaction.editReply(`✅ Deleted ${deleted.size} message(s)`);
+            const userMsgCount = new Collection();
+            deleted.forEach((msg) => {
+                const username = msg.author.username;
+                userMsgCount.set(username, (userMsgCount.get(username) || 0) + 1);
+            });
+            const embed = new EmbedBuilder().setTitle(`✔ deleted ${deleted.size} messages`)
+            embed.setColor("#DC143C");
+            let description = "";
+            userMsgCount.forEach((count, username) => {
+                description += `**${username}**: ${count} messages\n`;
+            });
+            await interaction.editReply({ embeds: [embed] });
+            embed.setDescription(description);
         } catch (error) {
             console.error("Bulk delete error:", error);
             await interaction.editReply("❌ Failed to delete messages. Make sure the messages are not older than 14 days and that I have the proper permissions.");
@@ -31,9 +43,30 @@ const invokeMsgCommand = async (message) => {
         return message.reply("Please provide a valid count between 1 and 100");
     }
     try {
-        await message.reply(`Deleting ${count} message(s)`);
+        await message.reply(`Deleting ${count} messages..`);
         const deleted = await message.channel.bulkDelete(count + 2);
-        const sentMessage = await message.channel.send(`Deleted ${count} message(s)`);
+        const userMsgCount = new Collection();
+        let i = 0;
+        deleted.forEach((msg) => {
+            if (i < 2) {
+                i++;
+                return;
+            }
+            const username = msg.author.username;
+            userMsgCount.set(username, (userMsgCount.get(username) || 0) + 1);
+        });
+
+        const embed = new EmbedBuilder().setTitle(`✔ deleted ${deleted.size - 2} messages`)
+        embed.setColor("#DC143C");
+        let description = "";
+        userMsgCount.forEach((count, username) => {
+            description += `**${username}**: ${count} messages\n`;
+        });
+
+        embed.setDescription(description);
+
+        const sentMessage = await message.channel.send({ embeds: [embed] });
+
         setTimeout(() => {
             sentMessage.delete();
         }, 2000);
